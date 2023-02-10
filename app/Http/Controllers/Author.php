645ad990641c3c0afa6fca9ced081625cd47b846
Diwa Perkasa <diwa.perkasa@gmail.com>
2023-02-10 20:03:52 +0700

    update code

--- app/Http/Controllers/Author.php
+++ app/Http/Controllers/Author.php
@@ -5,13 +5,12 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Author extends Controller
 {
     public function register(Request $request)
     {
-        // dd($request);
-
         $data = [
             'name'=> $request->name,
             'email'=> $request->email,
@@ -20,7 +19,7 @@ class Author extends Controller
 
         $user = User::create($data);
 
-        return redirect('/login');
+        return redirect('/register');
     }
 
     public function login(Request $request)
@@ -29,18 +28,28 @@ class Author extends Controller
             'email' => 'email|required',
             'password' => 'required'
         ]);
-// cek ulang
-        if (!auth()->attempt($data)) {
-            return response(['error_message' => 'Incorrect Details. 
-            Please try again']);
+        // cek ulang
+        if (!Auth::attempt($data)) {
+            return redirect("/login");
         }
 
-        $token = auth()->user()->createToken('API Token')->accessToken;
-
-        return response(['user' => auth()->user(), 'token' => $token]);
+        return redirect('/');
     }
+
     public function index()
     {
         return view('user');
+    }
+
+    public function loginPage()
+    {
+        return view('login');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        return redirect('login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,37 +16,44 @@ use App\Http\Controllers\Author;
 | contains the "web" middleware group. Now create something great!
 |
 */
- 
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [Mahasiswa::class, 'homepage']);
+    Route::get('/mahasiswa', [Mahasiswa::class, 'index']);
+
+    Route::get('/mahasiswa/create', [Mahasiswa::class, 'create']);
+    Route::post('/mahasiswa/create', [Mahasiswa::class, 'post']);
+    Route::get('/mahasiswa/edit/{id}', [Mahasiswa::class, 'edit']);
+    Route::post('/mahasiswa/edit/{id}', [Mahasiswa::class, 'postEdit']);
+    Route::post('/mahasiswa/delete/{id}', [Mahasiswa::class, 'delete']);
+
+    Route::get('/dosen', [Dosen::class, 'index']);
+    Route::get('/dosen/create', [Dosen::class, 'create']);
+    Route::post('/dosen/create', [Dosen::class, 'post']);
+    Route::get('/dosen/edit/{id}', [Dosen::class, 'edit']);
+    Route::post('/dosen/edit/{id}', [Dosen::class, 'postEdit']);
+    Route::post('/dosen/delete/{id}', [Dosen::class, 'delete']);
+
+    // prefix (untuk grup route)
+    // line 40-42 masukin route
+    Route::prefix('comment')->group(function () {
+        Route::get('/', [Comment::class, 'index']);
+        Route::post('/', [Comment::class, 'post']);
+        Route::post('/delete/{id}', [Comment::class, 'delete']);
+    });
+});
+
 Route::get('/greeting', function () {
     return 'Hello World';
 });
 
-Route::get('/mahasiswa', [Mahasiswa::class, 'index']); 
-Route::get('/mahasiswa/create', [Mahasiswa::class, 'create']);
-Route::post('/mahasiswa/create', [Mahasiswa::class, 'post']);
-Route::get('/mahasiswa/edit/{id}', [Mahasiswa::class, 'edit']);
-Route::post('/mahasiswa/edit/{id}', [Mahasiswa::class, 'postEdit']);
-Route::post('/mahasiswa/delete/{id}', [Mahasiswa::class, 'delete']);
-
-Route::get('/dosen', [Dosen::class, 'index']);
-Route::get('/dosen/create', [Dosen::class, 'create']);
-Route::post('/dosen/create', [Dosen::class, 'post']);
-Route::get('/dosen/edit/{id}', [Dosen::class, 'edit']);
-Route::post('/dosen/edit/{id}', [Dosen::class, 'postEdit']);
-Route::post('/dosen/delete/{id}', [Dosen::class, 'delete']);
-
-// prefix (untuk grup route)
-// line 40-42 masukin route
-Route::prefix('comment')->group(function () {
-    Route::get('/', [Comment::class, 'index']);
-Route::post('/', [Comment::class, 'post']);
-Route::post('/delete/{id}', [Comment::class, 'delete']);
-});
-
-Route::get('/', [Mahasiswa::class, 'homepage']);
 // Only authenticated users may access this route...
 // penyimpanan data server di session (cookies)
 // middleware = jembatan
 // Route::get('/login', function () {})->middleware('auth.basic');
-Route::get('/login', [Author::class, 'index']);
+Route::get('/register', [Author::class, 'index']);
 Route::post('/register', [Author::class, 'register']);
+
+Route::get('/login', [Author::class, 'loginPage']);
+Route::post('/login', [Author::class, 'login']);
+Route::get('/logout', [Author::class, 'logout']);
